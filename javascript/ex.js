57daed6b2bd85fb96f2ce79f66f257926c0cc200
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-09-23 18:50:48 +0530

    js

---
+++ javascript/ex.js
@@ -0,0 +1,15 @@
+console.log("hello this External js");
+console.log(6+278)
+console.log("sub =>",10-2)
+
+document.writeln("HEllo")
+document.writeln("<br>sum =>",10+20)
+document.writeln("<h4>hello</h4>")
+
+// 
+console.log(document.getElementById("h1"))
+
+document.getElementById("h1").innerText = "Hello javscript"
+
+document.getElementById("h1").innerHTML = "<i>Hello</i>"
+
