3ae494f3f92cd81b9f01463b88e041c5652aec95
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-09-24 18:45:36 +0530

    js

--- javascript/ex.js
+++ javascript/ex.js
@@ -1,15 +1,9 @@
-console.log("hello this External js");
-console.log(6+278)
-console.log("sub =>",10-2)
+console.log("hello")
 
-document.writeln("HEllo")
-document.writeln("<br>sum =>",10+20)
-document.writeln("<h4>hello</h4>")
+// bom :- browser object model
 
-// 
-console.log(document.getElementById("h1"))
-
-document.getElementById("h1").innerText = "Hello javscript"
-
-document.getElementById("h1").innerHTML = "<i>Hello</i>"
-
+// pop box
+// alert("pls field requir ...!");
+// confirm("Are your sure ? ");
+// prompt("5 + 5 ? ");
+// print()
