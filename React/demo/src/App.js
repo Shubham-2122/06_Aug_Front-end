4f20f11bae6a89cd10ad992cf64375a26ff46ca9
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-12-05 17:59:00 +0530

    react

--- React/demo/src/App.js
+++ React/demo/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Boots from "./Boots/Boots";
+import ReacData from "./Boots/ReacData";
+import Footer from "./Boots/Footer";
+import Login from "./Boots/Login";
 // import ClassCompo from "./Component/ClassCompo";
 // import FuncCompo from "./Component/FuncCompo";
 // import Hello from "./jsx/Hello";
 // import Style from "./style/Style";
 // import MainProps from "./Props/MainProps";
 // import MainState from "./State/MainState";
-import UseAx from "./Useeffect/UseAx";
-import UseCard from "./Useeffect/UseCard";
-import MainData from "./context/MainData";
+// import UseAx from "./Useeffect/UseAx";
+// import UseCard from "./Useeffect/UseCard";
+// import MainData from "./context/MainData";
 
 function App() {
     return (
@@ -32,7 +36,12 @@ function App() {
             {/* <UseCard /> */}
 
             {/* context */}
-            <MainData />
+            {/* <MainData /> */}
+
+            {/* <Boots /> */}
+            {/* <ReacData /> */}
+            {/* <Footer /> */}
+            {/* <Login /> */}
 
 
         </div>
---
+++ React/demo/src/Boots/Boots.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Boots() {
+  return (
+    <div>
+      <h1 className='bg-success text-light p-5'>Hello this boostrap</h1>
+    </div>
+  )
+}
+
+export default Boots
